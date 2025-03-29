ff8310e4fcf91842748a32a37fad2282fd216bb9
Eleventy8 <admin@eleventy8.com>
2025-03-29 17:32:40 +0000

    Added icon, although I'm not convinced

--- background.js
+++ background.js
@@ -13,3 +13,34 @@ chrome.runtime.onInstalled.addListener(() => {
       chrome.tabs.create({ url });
     }
   });
+
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "firegpt-search",
+      title: "Search with FireGPT",
+      contexts: ["selection"]
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "firegpt-search" && info.selectionText) {
+      const query = encodeURIComponent(info.selectionText);
+      const url = `https://chatgpt.com/?q=${query}&hints=search&ref=ext`;
+      chrome.tabs.create({ url });
+    }
+  });
+
+  chrome.omnibox.onInputEntered.addListener((text, disposition) => {
+    const query = encodeURIComponent(text);
+    const url = `https://chatgpt.com/?q=${query}&hints=search&ref=ext`;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      chrome.tabs.create({ url }, () => {
+        // Close the omnibox-activated blank tab
+        if (currentTab && currentTab.url === 'about:blank') {
+          chrome.tabs.remove(currentTab.id);
+        }
+      });
+    });
+  });
